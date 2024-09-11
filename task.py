c41d2e13697c20d5c85024bc01749764ae6d7a09
danielm-LCWC <144791876+danielm-LCWC@users.noreply.github.com>
2024-09-11 11:06:18 +0000

    Submission

--- task.py
+++ task.py
@@ -3,7 +3,7 @@ def task():
     result = ["Matthew", "Mark", "Luke"]
 
     # Code here
-    name = input("Enter your name")
+    name = input("Enter your name: ")
     result.append(name)
 
     # --------- #
